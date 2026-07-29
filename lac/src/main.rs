f92d59ecbd9474d4e7404294af709d9c9c584eeb
beeshrimp <cuiniao_nur.rs@proton.me>
2026-07-29 16:57:33 +0900

    init

---
+++ lac/src/main.rs
@@ -0,0 +1,107 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
+use std::sync::Arc;
+
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop, OwnedDisplayHandle},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowId},
+};
+
+mod renderer;
+use renderer::Renderer;
+
+#[derive(Default)]
+struct App {
+    renderer: Option<Renderer>,
+    time: f32,
+    input: f32,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        // ----------------------------------------------------------------------------------------- create window object
+        let window = Arc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        // ---------------------------------------------------------------------------------------------- create renderer
+        let renderer = pollster::block_on(Renderer::new(
+            event_loop.owned_display_handle(),
+            window.clone(),
+        ));
+        self.renderer = Some(renderer);
+        self.time = 0.0;
+        self.input = 0.0;
+
+        // ------------------------------------------------------------------------------------------------------- redraw
+        window.request_redraw();
+    }
+
+    // --------------------------------------------------------------------------------------------- handle window events
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        let renderer = self.renderer.as_mut().unwrap();
+
+        match event {
+            // --------------------------------------------------------------------------------------------- close window
+            WindowEvent::CloseRequested => {
+                println!("close requested; stopping");
+                event_loop.exit();
+            }
+            // --------------------------------------------------------------------------------------------------- redraw
+            WindowEvent::RedrawRequested => {
+                renderer.render();
+            }
+
+            // --------------------------------------------------------------------------------------------------- resize
+            WindowEvent::Resized(size) => {
+                // this event is always followed up by redraw request.
+                renderer.resize(size);
+            }
+            // ----------------------------------------------------------------------------------------- handle key input
+            WindowEvent::KeyboardInput { event, .. } => {
+                if let PhysicalKey::Code(code) = event.physical_key
+                    && event.state.is_pressed()
+                {
+                    match code {
+                        KeyCode::ArrowUp => {
+                            self.input += 0.005;
+                        }
+                        KeyCode::ArrowDown => {
+                            self.input -= 0.005;
+                        }
+                        _ => (),
+                    }
+                }
+            }
+
+            _ => (),
+        }
+    }
+
+    #[allow(unused_variables)]
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        let renderer = self.renderer.as_mut().unwrap();
+
+        self.time += 0.1;
+        renderer.update(self.time, self.input);
+
+        renderer.get_window().request_redraw()
+    }
+}
+
+fn main() {
+    env_logger::init();
+
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
---
+++ lac/src/shaders/effect.wgsl
@@ -0,0 +1,63 @@
+struct Locals {
+    time: f32,
+    input: f32}
+
+@group(0) @binding(0) var<uniform> locals: Locals;
+@group(0) @binding(1)
+var mid_texture: texture_2d<f32>;
+@group(0) @binding(2)
+var effect_sampler: sampler;
+
+struct VertexInput {
+    @location(0) position: vec2<f32>,
+    @location(1) uv: vec2<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>,
+    @location(0) uv: vec2<f32>,
+};
+
+@vertex
+fn vs_main(
+    model: VertexInput,
+) -> VertexOutput {
+    var out: VertexOutput;
+
+    out.position = vec4<f32>(model.position.xy, 0.0, 1.0);
+    out.uv = model.uv;
+    return out;
+}
+
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    let time = locals.time;
+    let input = locals.input;
+    let pos = in.position;
+    let uv = in.uv;
+
+    let seed = floor(uv.y * 100);
+
+    let line_rnd = rand(floor(time), seed);
+
+    let is_glitch = line_rnd >= 1.0 - input * 10;
+
+    let offset = select(0.0, rand(time, seed + 1.0), is_glitch);
+
+    var shifted_uv = uv;
+    shifted_uv.x += offset * input;
+
+    var colour = textureSample(mid_texture, effect_sampler, shifted_uv);
+
+    return vec4<f32>(colour);
+}
+
+fn rand(time: f32, uv_y: f32) -> f32 {
+    let seeds = vec2<f32>(time, uv_y);
+    let rand_p = fract(sin(dot(seeds, vec2(12.9898, 78.233))) * 43758.5453);
+    let rand_np = (rand_p * 2.0) - 1.0;
+    let randf = select(rand_p, rand_np, rand_p <= 0.02);
+
+    return randf;
+}
+
